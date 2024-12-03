27d18ae11f2301d49ad2353d105ab8804bb725ad
Yordan Valdiviezo <yordan.2511@gmail.com>
2024-12-03 15:29:49 -0500

    Cambio de consultas con JdbcTemplate a  ListCrudRespository

--- yordan-pizzeria/src/main/java/com/yordan/pizzeria/service/PizzaService.java
+++ yordan-pizzeria/src/main/java/com/yordan/pizzeria/service/PizzaService.java
@@ -1,22 +1,31 @@
 package com.yordan.pizzeria.service;
 
 import com.yordan.pizzeria.persistence.entity.PizzaEntity;
+import com.yordan.pizzeria.persistence.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class PizzaService {
-    private final JdbcTemplate jdbcTemplate;
-
+    //FORMA CON JdbcTemplate
+//    private final JdbcTemplate jdbcTemplate;
+//
+//    @Autowired
+//    public PizzaService(JdbcTemplate jdbcTemplate) {
+//        this.jdbcTemplate = jdbcTemplate;
+//    }
+//    public List<PizzaEntity> getAll(){
+//        return this.jdbcTemplate.query("SELECT * FROM pizza WHERE available=0",new BeanPropertyRowMapper<>(PizzaEntity.class));
+//    }
+    //Forma con Repository
+    private PizzaRepository pizzaRepository;
     @Autowired
-    public PizzaService(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public PizzaService(PizzaRepository pizzaRepository) {
+        this.pizzaRepository = pizzaRepository;
     }
     public List<PizzaEntity> getAll(){
-        return this.jdbcTemplate.query("SELECT * FROM pizza WHERE available=0",new BeanPropertyRowMapper<>(PizzaEntity.class));
+        return this.pizzaRepository.findAll();
     }
 }
